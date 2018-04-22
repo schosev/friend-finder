077b0c8be61053b0617c7de7a3d07e22646315d5
UMKC CTCFP User <umkc-ctcfpuser@umkc.edu>
2018-04-21 23:48:18 -0500

    Added matching logic, additional friends, and view html changes

--- data/friends.js
+++ data/friends.js
@@ -1,39 +1,30 @@
-// module.exports = function(app, path, bodyParser){
-
-//     app.use(bodyParser.urlencoded({ extended: true }));
-//     app.use(bodyParser.json());
 
     var friends = [
         {
             name: "Sarah",
             image: "https://i.pinimg.com/originals/8e/35/fb/8e35fb0ca2802166e97049a3477b6c3b.jpg",
-            scores: [1,4,3,5,2,3,4,2,5,3]
+            scores: [1,1,1,1,1,1,1,1,1,1]
         },
         {
             name: "Dan",
             image: "http://thehappyguy.com/wp-content/uploads/2011/11/funnyface.jpg",
-            scores: [1,3,3,2,2,3,1,2,5,3]
+            scores: [5,5,5,5,5,5,5,5,5,5]
+        },
+        {
+            name: "Ricky Racoon",
+            image: "https://orig00.deviantart.net/3642/f/2015/092/8/7/silly_face_by_big_bad_rocket-d8o3wnh.jpg",
+            scores: [3,2,4,5,3,2,2,3,1,2]
+        },
+        {
+            name: "Dave",
+            image: "https://funnypicture.org/wallpaper/2015/05/funny-celebrity-faces-6-cool-hd-wallpaper.jpg",
+            scores: [3,2,4,1,3,2,4,3,5,2]
+        },
+        {
+            name: "Sally",
+            image: "https://petapixel.com/assets/uploads/2013/01/The_ugly_truth_3-copy.jpg",
+            scores: [5,2,2,3,3,4,3,3,1,2]
         }
     ];
 
     module.exports = friends;
-
-//     app.get("/api/friends", function(req, res) {
-//         console.log("in display friends");
-//         return res.json(friends);
-//     });
-
-//     app.post("/api/friends", function(req, res) {
-//         var newFriend = req.body;
-    
-//         // Using a RegEx Pattern to remove spaces from newFriend
-//         //newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-
-//         console.log(newFriend);
-    
-//         friends.push(newFriend);
-    
-//         res.json(newFriend);
-//     });
-
-// }
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -5,22 +5,53 @@ module.exports = function(app, path, bodyParser){
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
 
+    var friendMatch = [];
+
+// Displays all characters
 app.get("/api/friends", function(req, res) {
-    console.log("in display friends");
     return res.json(friends);
+  });
+
+app.get("/api/friend", function(req, res) {
+    res.json(friendMatch);
 });
 
 app.post("/api/friends", function(req, res) {
     var newFriend = req.body;
 
-    // Using a RegEx Pattern to remove spaces from newFriend
-    //newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-
-    console.log(newFriend);
+    friendMatch = getMatch(newFriend, friends);
 
     friends.push(newFriend);
 
     res.json(newFriend);
 });
+
+
+function getMatch(newFriend, friends) {
+    var total = 100;
+    var matchTotal;
+    var match;
+    var newFriendIndvTotal;
+    
+    for (var z = 0; z < friends.length; z ++){
+        var matchArray = [];
+        for ( var i = 0; i < newFriend.scores.length; i++ ) {
+                matchIndv = Math.abs(parseInt(newFriend.scores[i]) - parseInt(friends[z].scores[i]));
+                matchArray.push(matchIndv);
+            }
+            var matchTotal = matchArray.reduce(add, 0);
+            function add(a, b) {
+                return a + b;
+            }
+            if (matchTotal < total) {
+                total = matchTotal;
+                match = friends[z];
+            }
+        
+    }  
+    return match;
+}
+
+
 
 }
